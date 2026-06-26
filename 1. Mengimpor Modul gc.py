dd4d668fa98ef28229243c3eeec888c1808218c8
rendhive <rendhivecode@gmail.com>
2026-06-27 00:17:04 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul gc.py

---
+++ 1. Mengimpor Modul gc.py
@@ -0,0 +1,5 @@
+import gc
+
+print("Modul gc berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk mengelola pengumpulan sampah (garbage collection) di Python.
+# Kondisi: Digunakan sebelum memanggil fungsi atau fitur dari modul gc.
