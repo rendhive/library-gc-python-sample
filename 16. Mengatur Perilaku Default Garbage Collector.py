a9df0850cb7f0117acc68f01811fa697119316b0
rendhive <rendhivecode@gmail.com>
2026-06-27 00:20:55 +0700

    [DOCT] Sample Library Python: 16. Mengatur Perilaku Default Garbage Collector.py

---
+++ 16. Mengatur Perilaku Default Garbage Collector.py
@@ -0,0 +1,5 @@
+import gc
+
+gc.set_free_list_limit(5)  # Atur batasan untuk free list
+# Fungsi: Mengatur properti pengumpulan untuk kontrol lebih besar.
+# Kondisi: Ketika Anda memerlukan pengaturan presisi atas manajemen memori.
