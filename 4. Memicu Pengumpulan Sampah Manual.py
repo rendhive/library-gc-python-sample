4b9e9d680f0e6f184971ba1ddf367a14b480dd38
rendhive <rendhivecode@gmail.com>
2026-06-27 00:17:56 +0700

    [DOCT] Sample Library Python: 4. Memicu Pengumpulan Sampah Manual.py

---
+++ 4. Memicu Pengumpulan Sampah Manual.py
@@ -0,0 +1,6 @@
+import gc
+
+gc.collect()  # Memicu pengumpulan sampah manual
+print("Pengumpulan sampah manual telah dipicu.")
+# Fungsi: Memicu proses pengumpulan sampah secara manual.
+# Kondisi: Ketika Anda ingin segera membersihkan objek yang tidak terpakai.
