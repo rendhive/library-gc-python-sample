4bbf7eb0b54b9fb440d67a5c28f8dd95b6bbf914
rendhive <rendhivecode@gmail.com>
2026-06-27 00:17:20 +0700

    [DOCT] Sample Library Python: 2. Melihat Status Pengumpulan Sampah.py

---
+++ 2. Melihat Status Pengumpulan Sampah.py
@@ -0,0 +1,6 @@
+import gc
+
+print("Status garbage collector saat ini:")
+print(gc.isenabled())
+# Fungsi: Mengecek apakah garbage collector dalam keadaan aktif.
+# Kondisi: Ketika Anda perlu mengetahui status saat ini dari pengumpulan sampah.
