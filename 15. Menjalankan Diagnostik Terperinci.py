8b6e3b1754be2010487cd149c57a40d10b536f95
rendhive <rendhivecode@gmail.com>
2026-06-27 00:20:39 +0700

    [DOCT] Sample Library Python: 15. Menjalankan Diagnostik Terperinci.py

---
+++ 15. Menjalankan Diagnostik Terperinci.py
@@ -0,0 +1,5 @@
+import gc
+
+gc.set_debug(gc.DEBUG_LEAK)  # Mengatur mode debug untuk mendeteksi kebocoran
+# Fungsi: Menggunakan fitur debugging untuk menemukan dan mendiagnosis kebocoran memori.
+# Kondisi: Ketika Anda perlu analisis mendalam tentang memori yang terpakai.
